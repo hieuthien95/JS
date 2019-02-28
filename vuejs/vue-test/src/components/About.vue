294aefc7ea2d95e3de4910fcfde2c4c6491c263b
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:32:09 +0700

    Create About.vue

---
+++ vuejs/vue-test/src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>What is about you?</h1>
+    <p>
+      This is all about me
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
