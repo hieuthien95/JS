c875195436a11e727f7f87a83ece843434367ce1
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:33:47 +0700

    Create meta-child-lv1-component.vue

---
+++ vuejs/vue-test/src/components/demo-component/meta-child-lv1-component.vue
@@ -0,0 +1,21 @@
+<template>
+	<li>
+		{{valu.name}}
+		<ComponentMetaChildLv2 :valu2='valu.lit'></ComponentMetaChildLv2>
+	</li>
+</template>
+
+<script>
+	import ComponentMetaChildLv2 from './meta-child-lv2-component'
+	
+	export default {
+	  props: ['valu'],
+	  components: { ComponentMetaChildLv2 },
+		
+		data: function() {
+			return {};
+		},
+		computed: {},
+		methods: {}
+	}
+</script>
