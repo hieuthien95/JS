b998886f3b6e6d7558947be13cf4abb70550faa0
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:33:56 +0700

    Create meta-child-lv2-component.vue

---
+++ vuejs/vue-test/src/components/demo-component/meta-child-lv2-component.vue
@@ -0,0 +1,19 @@
+<template>
+	<ol>
+		<span :key="index" v-for='(it, index) in valu2'><li>{{it}}</li></span>
+	</ol>
+</template>
+
+<script>
+	export default {
+		props: ['valu2'],
+	
+		data: function() {
+			return {};
+		},
+
+		computed: {},
+		
+		methods: {}
+	}
+</script>
