a36767678695430bb658f73c3517ff32f43dcb04
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:43:52 +0700

    Create const.js

---
+++ vuejs/vue-test/src/components/util/const.js
@@ -0,0 +1,3 @@
+export const HTTP_ACCESS_TOKEN = "access_token";
+// export const HTTP_PREFIX_URL_SUBMIT = "https://vue-thien-bh.firebaseio.com";
+export const HTTP_PREFIX_URL_SUBMIT = "http://localhost:3000";
