be3bb4a65088a59ac0edf63963c342a658514116
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:36:17 +0700

    Create component-process-bar.vue

---
+++ vuejs/vue-test/src/components/order/component-process-bar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div width="100%">
+        <div class="progress" style="background: #28a74566">
+            <div v-if="percentFunc > 0" class="progress-bar bg-success" :style="{width: percentFunc+'%', background: 'red'}">
+                {{percentFunc}}%
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    props: ['orderDetail'],
+    data: function() {
+        return { }
+    },
+    computed: {
+        percentFunc: function() {
+            var percentTmp = 0;
+            if (this.orderDetail.forWhat != null && this.orderDetail.forWhat != "") {
+                percentTmp += 25;
+            }
+            if (this.orderDetail.teamSelected != null && this.orderDetail.teamSelected != "") {
+                percentTmp += 25;
+            }
+            if (this.orderDetail.productSelected != null && this.orderDetail.productSelected.length != 0) {
+                percentTmp += 25;
+            }
+            if (this.orderDetail.userSelected != null && this.orderDetail.userSelected != "") {
+                percentTmp += 25;
+            }
+
+            return percentTmp;
+        }
+    }
+}
+</script>
