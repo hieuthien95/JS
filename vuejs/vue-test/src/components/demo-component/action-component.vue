4ee46b06a0f9a7438bf9cf39c0148b217000e098
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:33:20 +0700

    Create action-component.vue

---
+++ vuejs/vue-test/src/components/demo-component/action-component.vue
@@ -0,0 +1,15 @@
+<template>
+	<li><button @click="$emit('remove')">x</button> {{ name }}</li>
+</template>
+
+<script>
+	export default {
+		props: ['name'],
+		
+		data: function() {
+			return {};
+		},
+		computed: {},
+		methods: {}
+	}
+</script>
