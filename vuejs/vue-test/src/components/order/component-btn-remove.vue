9aeadea544e9669b0149a37f7b66aec3f8e5a884
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:35:46 +0700

    Create component-btn-remove.vue

---
+++ vuejs/vue-test/src/components/order/component-btn-remove.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<input style="margin-top: 3px" class="btn btn-danger" type="button" @click="$emit('remove')" value="x" /> 
+		{{valItem}}
+	</div>
+</template>
+
+<script>	
+	export default {
+		props: ['valItem']
+	}
+</script>
