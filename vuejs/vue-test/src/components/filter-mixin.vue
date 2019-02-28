490c7895a446b8928d94c225c3ef108da2875d4d
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:32:25 +0700

    Create filter-mixin.vue

---
+++ vuejs/vue-test/src/components/filter-mixin.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input v-model="teamInput"/>
+        <h1>{{ teamInput | addDotAfter }}</h1>
+        <h1>{{ 'Global: ' + teamInput | toUppercaseFilter }}</h1>
+        <hr>
+
+        <li v-for="(item, index) in teamInitCpt" :key="index">
+            {{ item.name }}
+        </li>
+    </div>
+</template>
+
+<script>
+import { teamInitMixin } from './util/storeMixin.js';
+
+export default {
+    mixins: [ teamInitMixin ],
+
+    filters: {
+        addDotAfter: function(text) {
+            if(text == '') {
+                return text;
+            }
+            return text + ".";
+        }
+    },
+}
+</script>
