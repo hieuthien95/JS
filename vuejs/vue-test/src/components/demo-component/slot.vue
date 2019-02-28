108e22b80c386254cf1edd92d0cc86d4dd357ba1
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:34:18 +0700

    Create slot.vue

---
+++ vuejs/vue-test/src/components/demo-component/slot.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+		
+	  Title in parent 
+	  <h4>{{title}}</h4> 
+	  <p>{{content}}</p>
+	  <hr>
+
+	  <child-component>
+		  <h4 slot="title">{{title}}</h4>
+		  <p>{{content}}</p>
+	  </child-component>
+
+	</div>
+</template>
+
+<script>	
+	import Vue from 'vue'
+	const busEvent = new Vue();
+
+	export default {
+		data() {
+			return {
+				title: "Car",
+				content: "This is a content, under Title, using Slot"
+			}
+		}, 
+		components: {
+			'child-component': {
+				template: `
+					<span>
+						Title in child
+
+		  				<!-- The slot name -->
+						<slot name="title"/>
+
+		  				<!-- The slot default -->
+						  <slot name="content">The slot default</slot>
+						  
+		  				<!-- The default slot -->
+						<slot/>
+					</span>
+					`,
+			},
+		}
+	}
+</script>
