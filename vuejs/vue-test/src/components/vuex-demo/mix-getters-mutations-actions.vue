ff4aff01647129dc3ca8a970803b43984f5a82da
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:44:50 +0700

    Create mix-getters-mutations-actions.vue

---
+++ vuejs/vue-test/src/components/vuex-demo/mix-getters-mutations-actions.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>VUEX</h1>
+        count: {{ countByState }} <br>
+        <button @click="countState">Increate</button>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+    methods: {
+        countState: function() {
+            this.$store.state.count++;
+        },
+        // ...mapState([
+        //     // map this.count to store.state.count
+        //     'count'
+        // ])
+    },
+    computed: {
+        countByState: function() {
+            return this.$store.state.count
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
