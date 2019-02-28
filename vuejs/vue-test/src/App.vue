e78300750b8d299461a40308c8fc21a75fd14d1d
hieuthien95 <hieuthien95@gmail.com>
2019-02-28 09:29:49 +0700

    Update App.vue

--- vuejs/vue-test/src/App.vue
+++ vuejs/vue-test/src/App.vue
@@ -1,60 +1,63 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+	  	<!-- <router-view name="header-layout"> -->
+		<header-layout/>
+
+		<div class="container">
+			<transition name="slide" mode="out-in">
+				<router-view></router-view>
+			</transition>
+		</div>
+	
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+	import HeaderLayout from './header.vue'
+
+	export default {
+		name: 'app',
+		components: {
+			'header-layout': HeaderLayout,
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+	.slide-enter {
+		opacity: 0;
+		transform: translateX(100px)
+	}
+	.slide-enter-active {
+    	transition: opacity .25s;
+		animation: slide-innn .25s ease-out;
+	}
 
-h1, h2 {
-  font-weight: normal;
-}
+	.slide-leave {
+		opacity: 1;
+		transform: translateX(0)
+	}
+	.slide-leave-active {
+		opacity: 0;
+		transition: opacity .25s ease-out;
+		animation: slide-outtt .25s ease-out;
+	}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+	@keyframes slide-innn {
+		0% {
+			transform: translateX(-100px);
+		}		
+		100% {
+			transform: translateX(0px);
+		}
+	}
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+	@keyframes slide-outtt {
+		0% {
+			transform: translateX(0px);
+		}
+		100% {
+			transform: translateX(100px);
+		}
+	}
 </style>
